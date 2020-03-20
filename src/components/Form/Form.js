4c294cc2c4cac58ac4b729f879ce37acfb9090c9
DavideDeFeudis <davidedefeudis@gmail.com>
2020-03-20 13:51:48 +0100

    Use react-hook-form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,115 +1,188 @@
-import React, { useState, useEffect } from 'react'
-import './Form.scss';
-import SpinnerModal from '../SpinnerModal/SpinnerModal'
+import React, { useState, useEffect } from "react";
+import "./Form.scss";
+import SpinnerModal from "../SpinnerModal/SpinnerModal";
+import { useForm } from "react-hook-form";
 
 export default function ContactForm() {
-    const initialState = {
-        anrede: '',
-        name: '',
-        email: '',
-        anfrage: '',
-        beschreibungstext: '',
-        datenschutz: false
-    }
-    const [formData, setFormData] = useState(initialState)
-    const [feedback, setFeedback] = useState('')
-    const [loading, setLoading] = useState(false)
-    const [isVisible, setIsVisible] = useState(false)
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    // fetch(`${process.env.REACT_APP_BACKEND_HOST}/user`, {
+    //     method: "POST",
+    //     body: JSON.stringify(data),
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     }
+    // })
+    // .then(response => response.json())
+    // .then(json => {
+    //     console.log(json)
+    //     setLoading(false)
+    //     setFeedback(json.success ? 'Ihre Anfrage wurde erfolgreich gesendet.' : 'Fehler beim Senden Ihrer Anfrage.')
+    //     // setFormData(initialState)
 
-    useEffect(() => {
-        if (formData.anfrage === 'Option 2') {
-            setIsVisible(true)
-        } else {
-            setIsVisible(false)
-            setFormData({
-                ...formData,
-                beschreibungstext: '' // clear field if user selects option 1 or 3 after having filled it
-            })
-        }
-    }, [formData.anfrage])
+    // })
+    // .catch(err => {
+    //     console.log('Fehler beim Senden Ihrer Anfrage:', err)
+    //     setLoading(false)
+    //     setFeedback('Fehler beim Senden Ihrer Anfrage.')
+    // })
+  };
 
-    const handleChange = e => {
-        const isCheckBox = e.target.type === 'checkbox'
-        setFormData({
-            ...formData,
-            [e.target.name]: isCheckBox ? e.target.checked : e.target.value
-        })
-        if (feedback) setFeedback('')
-    }
+  const [feedback, setFeedback] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false); // beschreibungstext
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        setLoading(true)
-        sendFormData()
-    }
+  // useEffect(() => {
+  //     if (formData.anfrage === 'Option 2') {
+  //         setIsVisible(true)
+  //     } else {
+  //         setIsVisible(false)
+  //         // setFormData({
+  //         //     ...formData,
+  //         //     beschreibungstext: '' // clear field if user selects option 1 or 3 after having filled it
+  //         // })
+  //     }
+  // }, []) // need state for anfrage? and for text to clear it?
 
-    const sendFormData = () => {
-        fetch(`${process.env.REACT_APP_BACKEND_HOST}/user`, {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
-                setLoading(false)
-                setFeedback(json.success ? 'Ihre Anfrage wurde erfolgreich gesendet.' : 'Fehler beim Senden Ihrer Anfrage.')
-                setFormData(initialState)
+  // const handleChange = e => {
+  //     const isCheckBox = e.target.type === 'checkbox'
+  //     setFormData({
+  //         ...formData,
+  //         [e.target.name]: isCheckBox ? e.target.checked : e.target.value
+  //     })
+  //     if (feedback) setFeedback('')
+  // }
 
-            })
-            .catch(err => {
-                console.log('Fehler beim Senden Ihrer Anfrage:', err)
-                setLoading(false)
-                setFeedback('Fehler beim Senden Ihrer Anfrage.')
-            })
-    }
+  // const handleSubmit = e => {
+  //     e.preventDefault()
+  //     setLoading(true)
+  //     sendFormData()
+  // }
 
-    return (
-        <div className="container">
-            <form role="form" data-toggle="validator" onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="inputAnrede" className="label">Anrede*</label>
-                    <input pattern="(.|\s)*\S(.|\s)*" data-error="Invalid" name="anrede" type="text" className="form-control" id="inputAnrede" placeholder="Anrede" onChange={handleChange} value={formData.anrede} required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="inputName" className="label">Name*</label>
-                    <input name="name" type="text" className="form-control" id="inputName" placeholder="Name" onChange={handleChange} value={formData.name} required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="inputEmail" className="label">Email*</label>
-                    <input name="email" type="email" className="form-control" id="inputEmail" placeholder="Email" onChange={handleChange} value={formData.email} required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleFormControlSelect1" className="label">Anfrage*</label>
-                    <select name="anfrage" className="form-control" id="exampleFormControlSelect1" onChange={handleChange} value={formData.anfrage} required>
-                        <option value="">--Wählen Sie bitte eine Option--</option>
-                        <option>Option 1</option>
-                        <option>Option 2</option>
-                        <option>Option 3</option>
-                    </select>
-                </div>
-                {
-                    isVisible &&
-                    <div className="form-group">
-                        <label htmlFor="inputBeschreibungstext" className="label">Beschreibungstext</label>
-                        <input name="beschreibungstext" type="text" className="form-control" id="inputBeschreibungstext" placeholder="Beschreibungstext" onChange={handleChange} value={formData.beschreibungstext} required />
-                    </div>
-                }
-                <div className="form-group">
-                    <div className="">Datenschutz Zustimmung*</div>
-                    <div className="form-check">
-                        <input name="datenschutz" className="form-check-input" type="checkbox" id="gridCheck1" onChange={handleChange} value={formData.datenschutz} required />
-                        <label className="form-check-label" htmlFor="gridCheck1">Mit dem Absenden stimme ich den Datenschutzbestimmungen zu.</label>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <button type="submit" className="btn btn-secondary">Absenden</button>
-                    <p className='my-3'>{feedback}</p>
-                </div>
-            </form>
-            <SpinnerModal loading={loading} fullScreen />
-        </div >
-    )
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label htmlFor="inputAnrede" className="label">
+            Anrede*
+          </label>
+          <input
+            pattern="(.|\s)*\S(.|\s)*"
+            data-error="Invalid"
+            name="anrede"
+            type="text"
+            className="form-control"
+            id="inputAnrede"
+            placeholder="Anrede"
+            ref={register}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputName" className="label">
+            Name*
+          </label>
+          <input
+            name="name"
+            type="text"
+            className="form-control"
+            id="inputName"
+            placeholder="Name"
+            ref={register({
+              required: "Pflichtfeld.",
+              minLength: {
+                value: 2,
+                message: "Name muss mindestens 8 Zeichen lang sein."
+              }
+            })}
+          />
+        </div>
+        {errors.name && <p>{errors.name.message}</p>}
+        <div className="form-group">
+          <label htmlFor="inputEmail" className="label">
+            Email*
+          </label>
+          <input
+            name="email"
+            type="email"
+            className="form-control"
+            id="inputEmail"
+            placeholder="E-Mail"
+            ref={register({
+              required: "Pflichtfeld.",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Geben Sie bitte eine gültige E-Mail-Adresse an."
+              }
+            })}
+          />
+        </div>
+        {errors.email && <p>{errors.email.message}</p>}
+        <div className="form-group">
+          <label htmlFor="exampleFormControlSelect1" className="label">
+            Anfrage*
+          </label>
+          <select
+            name="anfrage"
+            className="form-control"
+            id="exampleFormControlSelect1"
+            ref={register({
+              required: "Pflichtfeld."
+            })}
+          >
+            <option value="">--Wählen Sie bitte eine Option--</option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+          </select>
+        </div>
+        {errors.anfrage && <p>{errors.anfrage.message}</p>}
+        {isVisible && (
+          <div className="form-group">
+            <label htmlFor="inputBeschreibungstext" className="label">
+              Beschreibungstext
+            </label>
+            <input
+              name="beschreibungstext"
+              type="text"
+              className="form-control"
+              id="inputBeschreibungstext"
+              placeholder="Beschreibungstext"
+              ref={register({
+                required: "Pflichtfeld."
+              })}
+            />
+            {errors.beschreibungstext && (
+              <p>{errors.beschreibungstext.message}</p>
+            )}
+          </div>
+        )}
+        <div className="form-group">
+          <div className="">Datenschutz Zustimmung*</div>
+          <div className="form-check">
+            <input
+              name="datenschutz"
+              className="form-check-input"
+              type="checkbox"
+              id="gridCheck1"
+              ref={register({
+                required: "Pflichtfeld."
+              })}
+            />
+            <label className="form-check-label" htmlFor="gridCheck1">
+              Mit dem Absenden stimme ich den Datenschutzbestimmungen zu.
+            </label>
+          </div>
+          {errors.datenschutz && <p>{errors.datenschutz.message}</p>}
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-secondary">
+            Absenden
+          </button>
+          <p className="my-3">{feedback}</p>
+        </div>
+      </form>
+      <SpinnerModal loading={loading} fullScreen />
+    </div>
+  );
 }
