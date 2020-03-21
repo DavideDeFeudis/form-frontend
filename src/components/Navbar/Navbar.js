dbc0a3814840f39fc6c459118b5cdabf1a0573ff
DavideDeFeudis <davidedefeudis@gmail.com>
2020-03-21 10:00:07 +0100

    Refactor logic in users

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand navbar-dark bg-light">
+    <nav className="navbar navbar-expand navbar-light bg-light">
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -6,21 +6,20 @@ import "./Users.scss";
 
 export default function Users() {
   const url = process.env.REACT_APP_BACKEND_HOST + "/users";
-  const [isLoading, fetchedData] = useHttp(url, []);
-
+  let [isLoading, fetchedData] = useHttp(url, []);
   let content = (
     <div className="fetching-spinner container text-center">
-      <img src={loadingGif} width="40" height="40" alt="Users werden geladen..." />
+      <img
+        src={loadingGif}
+        width="40"
+        height="40"
+        alt="Users werden geladen..."
+      />
       <p className="mt-3">Users werden geladen...</p>
     </div>
   );
-  const usersList = fetchedData
-    ? fetchedData.map(user => {
-        return <User key={user._id} user={user} />;
-      })
-    : [];
   if (!isLoading && fetchedData && fetchedData.length > 0) {
-    content = usersList;
+    content = fetchedData.map(user => <User key={user._id} user={user} />);
   } else if (!isLoading && !fetchedData) {
     content = <p className="my-5">Ein Fehler ist aufgetreten</p>;
   } else if (!isLoading && fetchedData.length === 0) {
